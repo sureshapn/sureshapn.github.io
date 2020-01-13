7af818b43b63540a87a04c9fd21b93f5f871e51c
SUBRAMANIAN <sureshapn.m@gmail.com>
2020-01-13 09:58:21 +0530

    Add files via upload

---
+++ precache-manifest.3118c14e6d38fc73045af578389aa230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c10462f0620ccef1d6f61a49ce25047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70344b52d92bc2b5c5e8",
+    "url": "/static/css/main.a8c3dbfe.chunk.css"
+  },
+  {
+    "revision": "c0f458a116ce237895a0",
+    "url": "/static/js/2.f7b07dca.chunk.js"
+  },
+  {
+    "revision": "dd897ee3cfa67e5c2ebba71b5eb1cda6",
+    "url": "/static/js/2.f7b07dca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70344b52d92bc2b5c5e8",
+    "url": "/static/js/main.89a9e123.chunk.js"
+  },
+  {
+    "revision": "e46f94403d4d91bf50a6",
+    "url": "/static/js/runtime-main.d4b32197.js"
+  }
+]);
